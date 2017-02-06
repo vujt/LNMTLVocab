6d5980e5cdfe75d92e387e49f5d69b412f754344
Jack <Jack@doe.com>
2017-02-05 18:57:33 -0800

    initial

--- LNMTVocab/LNMTVocab/Controllers/HomeController.cs
+++ LNMTVocab/LNMTVocab/Controllers/HomeController.cs
@@ -4,13 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using LNMTVocab.Data;
+
 namespace LNMTVocab.Controllers
 {
+    
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
-            return View();
+            var myList = ReadFile();
+
+            return View(myList);
+        }
+
+        
+        private List<Glossary> ReadFile()
+        {
+         //   string path1 = string.Format("{0}/{1}", Server.MapPath("~/Data/"), "translation.txt");
+            var root = AppDomain.CurrentDomain.BaseDirectory;
+            List<Glossary> myList = new List<Glossary>();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(root + "\\Data\\translation.txt");
+            string fileLines;
+
+            
+                while ((fileLines = file.ReadLine()) != null)
+                {
+
+                    string[] elements;
+                    elements = fileLines.Split(new char[] { '\t' });
+
+                    myList.Add(new Glossary() { vocab = elements[1], meaning = elements[2], vocabLength = elements[4] });
+                    
+
+                }
+
+            return myList;
+
         }
 
         public ActionResult About()
---
+++ LNMTVocab/LNMTVocab/Data/Glossary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LNMTVocab.Data
+{
+    public class Glossary
+    {
+        public string meaning { get; set; }
+        public string vocab { get; set; }
+        public string vocabLength { get; set; }
+    }
+}
